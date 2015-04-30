93a548efd0ea599fc54c6e38a321ea1862191d8a
Ned Twigg <ned.twigg@diffplug.com>
2015-04-29 20:01:57 -0700

    I can't get Spotless to run on itself for the life of me.  But, I can make a unit test that calls SelfTest manually.  It achieves the same thing, and lets us step through the code if we'd like to.

---
+++ src/test/java/com/diffplug/gradle/spotless/SelfTest.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2015 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.gradle.spotless;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+import org.gradle.api.Project;
+import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.Test;
+
+/**
+ * Running spotless on ourselves yields the following error:
+ * 
+ * Module version com.diffplug.gradle.spotless:spotless:1.0-SNAPSHOT,
+ * configuration 'classpath' declares a dependency on configuration
+ * 'default' which is not declared in the module descriptor for
+ * com.diffplug.gradle.spotless:spotless:1.0-SNAPSHOT
+ * 
+ * Tried all kinds of things to fix it, no luck so far.
+ * 
+ * So, we'll just run it from inside of ourselves.
+ */
+public class SelfTest {
+	/** Runs the check (which we want to happen in the test suite). */
+	@Test
+	public void check() throws Exception {
+		runTasksWithCheck(true);
+	}
+
+	/** Applies the format (which should be manual). */
+	public static void main(String[] args) throws Exception {
+		runTasksWithCheck(false);
+	}
+
+	/** Returns all of the FormatTasks which have check equal to the given value. */
+	private static void runTasksWithCheck(boolean check) throws Exception {
+		Project project = createProject(extension -> {
+			extension.java(java -> {
+				java.target("**/*.java");
+				java.licenseHeaderFile("spotless.license.java");
+				java.importOrderFile("spotless.importorder.properties");
+				java.eclipseFormatFile("spotless.eclipseformat.xml");
+				java.customLazy("Lambda fix", () -> raw -> raw.replace("})", "})").replace("},", "},"));
+			} );
+			extension.format("misc", misc -> {
+				misc.target("**/*.gradle", "**/*.md", "**/*.gitignore");
+				misc.indentWithTabs();
+				misc.trimTrailingWhitespace();
+				misc.endWithNewline();
+			} );
+		} );
+		project.getTasks().stream()
+				.filter(task -> task instanceof FormatTask)
+				.map(task -> (FormatTask) task)
+				.filter(task -> task.check == check)
+				.forEach(task -> {
+					try {
+						task.format();
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				} );
+	}
+
+	/** Creates a Project which has had the SpotlessExtension setup. */
+	private static Project createProject(Consumer<SpotlessExtension> test) throws Exception {
+		Project project = ProjectBuilder.builder().withProjectDir(new File("").getAbsoluteFile()).build();
+		// create the spotless plugin
+		SpotlessPlugin plugin = project.getPlugins().apply(SpotlessPlugin.class);
+		// setup the plugin
+		test.accept(plugin.getExtension());
+		plugin.createTasks();
+		// return the configured plugin
+		return project;
+	}
+}
