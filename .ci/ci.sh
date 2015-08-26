c1a14b5db39dde056e4bf7481140207a5027f318
Ned Twigg <ned.twigg@diffplug.com>
2015-08-26 07:00:35 -0700

    Another shot at getting CI to upload.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -5,5 +5,5 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "diffplug/spotless" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Publish the artifacts
-	./gradlew publishPlugins || exit 1
+	./gradlew publish publishPlugins -Dgradle.publish.key=$gradle.publish.key -Dgradle.publish.secret=$gradle.publish.secret || exit 1
 fi
