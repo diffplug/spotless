73045cc88c49372eb6d8e64ab0f0e34915f46146
Ned Twigg <ned.twigg@diffplug.com>
2016-10-24 15:08:37 -0700

    First cut at a cycle detector for checking that F(F(input)) == F(input).

---
+++ src/main/java/com/diffplug/gradle/spotless/PaddedCell.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.gradle.spotless;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+import com.diffplug.common.base.Preconditions;
+import com.diffplug.common.base.Unhandled;
+
+class PaddedCell {
+	private final Formatter formatter;
+	private final File file;
+	private final String original;
+
+	PaddedCell(Formatter formatter, File file) throws IOException {
+		this.formatter = formatter;
+		this.file = file;
+		byte[] rawBytes = Files.readAllBytes(file.toPath());
+		String raw = new String(rawBytes, StandardCharsets.UTF_8);
+		this.original = LineEnding.toUnix(raw);
+	}
+
+	private static final int MAX_CYCLE = 10;
+
+	public Result cycle() {
+		return cycle(MAX_CYCLE);
+	}
+
+	public Result cycle(int maxLength) {
+		Preconditions.checkArgument(maxLength >= 2, "maxLength must be at least 2");
+		String appliedOnce = formatter.applyAll(original, file);
+		if (appliedOnce.equals(original)) {
+			return Result.converges(appliedOnce, 0);
+		}
+
+		String appliedTwice = formatter.applyAll(appliedOnce, file);
+		if (appliedOnce.equals(appliedTwice)) {
+			return Result.converges(appliedOnce, 1);
+		}
+
+		List<String> appliedN = new ArrayList<>();
+		appliedN.add(appliedOnce);
+		appliedN.add(appliedTwice);
+		String input = appliedTwice;
+		while (appliedN.size() < maxLength) {
+			String output = formatter.applyAll(input, file);
+			if (output.equals(input)) {
+				return Result.converges(output, appliedN.size());
+			} else {
+				int idx = appliedN.indexOf(output);
+				if (idx >= 0) {
+					if (idx == appliedN.size() - 1) {
+						return Result.converges(output, idx);
+					} else {
+						return Result.cycle(appliedN.subList(idx, appliedN.size()));
+					}
+				} else {
+					appliedN.add(output);
+					input = output;
+				}
+			}
+		}
+		return Result.diverges(maxLength);
+	}
+
+	public static final class Result {
+		private final Object result;
+
+		public static Result cycle(List<String> result) {
+			return new Result(new Cycle(result));
+		}
+
+		public static Result converges(String to, int after) {
+			return new Result(new Converge(to, after));
+		}
+
+		public static Result diverges(int after) {
+			return new Result(new Diverge(after));
+		}
+
+		private Result(Object result) {
+			this.result = Objects.requireNonNull(result);
+		}
+
+		public <T> T applyCycleConvergeDiverge(Function<Cycle, ? extends T> cycle, Function<Converge, ? extends T> converge, Function<Diverge, ? extends T> diverge) {
+			if (result instanceof Cycle) {
+				return cycle.apply((Cycle) result);
+			} else if (result instanceof Converge) {
+				return converge.apply((Converge) result);
+			} else if (result instanceof Diverge) {
+				return diverge.apply((Diverge) result);
+			} else {
+				throw Unhandled.classException(result);
+			}
+		}
+
+		@SuppressWarnings("unchecked")
+		public <T> T as(Class<T> clazz) {
+			if (is(clazz)) {
+				return (T) result;
+			} else {
+				throw new IllegalArgumentException("This is " + result.getClass() + ", not " + clazz);
+			}
+		}
+
+		public boolean is(Class<?> clazz) {
+			return result.getClass().equals(clazz);
+		}
+	}
+
+	public static final class Cycle {
+		List<String> result;
+
+		Cycle(List<String> result) {
+			this.result = result;
+		}
+
+		public List<String> getCycle() {
+			return result;
+		}
+	}
+
+	public static final class Converge {
+		String convergesTo;
+		int after;
+
+		Converge(String convergesTo, int after) {
+			this.convergesTo = convergesTo;
+			this.after = after;
+		}
+
+		public String convergesTo() {
+			return convergesTo;
+		}
+
+		public int afterIterations() {
+			return after;
+		}
+	}
+
+	public static final class Diverge {
+		final int after;
+
+		Diverge(int after) {
+			this.after = after;
+		}
+
+		public int afterIterations() {
+			return after;
+		}
+	}
+}
---
+++ src/test/java/com/diffplug/gradle/spotless/PaddedCellTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.gradle.spotless;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.diffplug.common.base.Throwing;
+import com.diffplug.gradle.spotless.PaddedCell.*;
+
+public class PaddedCellTest {
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+
+	private <T> T testCase(Throwing.Function<String, String> step, String input, Class<T> expectedOutputType) throws IOException {
+		List<FormatterStep> steps = new ArrayList<>();
+		steps.add(FormatterStep.create("step", step));
+		Formatter formatter = new Formatter(LineEnding.UNIX_POLICY, folder.getRoot().toPath(), steps);
+
+		File file = folder.newFile();
+		Files.write(file.toPath(), input.getBytes(StandardCharsets.UTF_8));
+
+		PaddedCell padded = new PaddedCell(formatter, file);
+		PaddedCell.Result result = padded.cycle();
+		return result.as(expectedOutputType);
+	}
+
+	@Test
+	public void wellBehaved() throws IOException {
+		Converge alreadyClean = testCase(input -> input, "CCC", Converge.class);
+		Assert.assertEquals("CCC", alreadyClean.convergesTo());
+		Assert.assertEquals(0, alreadyClean.afterIterations());
+
+		Converge oneIteration = testCase(input -> "A", "CCC", Converge.class);
+		Assert.assertEquals("A", oneIteration.convergesTo());
+		Assert.assertEquals(1, oneIteration.afterIterations());
+	}
+
+	@Test
+	public void pingPong() throws IOException {
+		Cycle cycle = testCase(input -> {
+			return input.equals("A") ? "B" : "A";
+		}, "CCC", Cycle.class);
+		Assert.assertEquals(Arrays.asList("A", "B"), cycle.getCycle());
+	}
+
+	@Test
+	public void fourState() throws IOException {
+		Cycle cycle = testCase(input -> {
+			// @formatter:off
+			switch (input) {
+			case "A": return "B";
+			case "B": return "C";
+			case "C": return "D";
+			default:  return "A";
+			}
+			// @formatter:on
+		}, "CCC", Cycle.class);
+		Assert.assertEquals(Arrays.asList("A", "B", "C", "D"), cycle.getCycle());
+	}
+
+	@Test
+	public void converging() throws IOException {
+		Converge converge = testCase(input -> {
+			if (input.isEmpty()) {
+				return input;
+			} else {
+				return input.substring(0, input.length() - 1);
+			}
+		}, "CCC", Converge.class);
+		Assert.assertEquals("", converge.convergesTo());
+		Assert.assertEquals(3, converge.afterIterations());
+	}
+
+	@Test
+	public void diverging() throws IOException {
+		Diverge diverge = testCase(input -> {
+			return input + " ";
+		}, "", Diverge.class);
+		Assert.assertEquals(10, diverge.afterIterations());
+	}
+}
