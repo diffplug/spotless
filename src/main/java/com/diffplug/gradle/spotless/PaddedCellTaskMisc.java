e09822a09aa020098d2f8be32d2b25d4e4bcb559
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2016-11-07 18:38:29 +0000

    Upgrade to Gradle 3.1

--- src/main/java/com/diffplug/gradle/spotless/PaddedCellTaskMisc.java
+++ src/main/java/com/diffplug/gradle/spotless/PaddedCellTaskMisc.java
@@ -118,7 +118,7 @@ class PaddedCellTaskMisc {
 	}
 
 	private static Path diagnoseDir(CheckFormatTask task) {
-		return task.getProject().getBuildDir().toPath().resolve("spotless-diagnose-" + task.getFormatName());
+		return task.getProject().getBuildDir().toPath().resolve("spotless-diagnose-" + task.formatName());
 	}
 
 	@SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
