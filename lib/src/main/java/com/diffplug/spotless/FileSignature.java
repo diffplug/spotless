985bfc3979a9412ca2b43f39b0e586a4300af60c
Ned Twigg <ned.twigg@diffplug.com>
2016-11-30 12:48:22 -0800

    FileSignature no longer needs ImmutableSortedSet.

--- lib/src/main/java/com/diffplug/spotless/FileSignature.java
+++ lib/src/main/java/com/diffplug/spotless/FileSignature.java
@@ -18,10 +18,12 @@ package com.diffplug.spotless;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
-
-import com.diffplug.common.collect.ImmutableSortedSet;
-import com.diffplug.common.collect.Iterables;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
@@ -35,7 +37,7 @@ public final class FileSignature implements Serializable {
 	 * incremental builds.
 	 */
 	@SuppressFBWarnings("SE_TRANSIENT_FIELD_NOT_RESTORED")
-	private final transient ImmutableSortedSet<File> sortedFiles;
+	private final transient List<File> sortedFiles;
 
 	private final String[] filenames;
 	private final long[] filesizes;
@@ -46,7 +48,7 @@ public final class FileSignature implements Serializable {
 	}
 
 	public FileSignature(Iterable<File> files) throws IOException {
-		sortedFiles = ImmutableSortedSet.copyOf(files);
+		sortedFiles = toSortedSet(files);
 
 		filenames = new String[sortedFiles.size()];
 		filesizes = new long[sortedFiles.size()];
@@ -62,12 +64,46 @@ public final class FileSignature implements Serializable {
 	}
 
 	/** Returns all of the files in this signature, throwing an exception if there are more or less than 1 file. */
-	public ImmutableSortedSet<File> files() {
-		return sortedFiles;
+	public Collection<File> files() {
+		return Collections.unmodifiableList(sortedFiles);
 	}
 
 	/** Returns the only file in this signature, throwing an exception if there are more or less than 1 file. */
 	public File getOnlyFile() {
-		return Iterables.getOnlyElement(sortedFiles);
+		if (sortedFiles.size() == 1) {
+			return sortedFiles.get(0);
+		} else {
+			throw new IllegalArgumentException("Expected one file, but was " + sortedFiles.size());
+		}
+	}
+
+	/** Returns a "sortedSet" by copying the input to an ArrayList, sorting, and removing duplicates. */
+	static <T extends Comparable<T>> List<T> toSortedSet(Iterable<T> unsorted) {
+		// copy the whole unsorted into a list
+		List<T> result;
+		if (unsorted instanceof Collection) {
+			result = new ArrayList<>((Collection<T>) unsorted);
+		} else {
+			result = new ArrayList<>();
+			for (T element : unsorted) {
+				result.add(element);
+			}
+		}
+		// sort it
+		Collections.sort(result);
+		// remove any duplicates (normally there won't be any)
+		if (result.size() > 1) {
+			Iterator<T> iter = result.iterator();
+			T last = iter.next();
+			while (iter.hasNext()) {
+				T next = iter.next();
+				if (next.compareTo(last) == 0) {
+					iter.remove();
+				} else {
+					last = next;
+				}
+			}
+		}
+		return result;
 	}
 }
