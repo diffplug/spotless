07ad265ca3f7b294db47013ac5269c3b25b0285b
Ned Twigg <ned.twigg@diffplug.com>
2016-12-02 14:59:04 -0800

    Removed unused import from PaddedCellBulk.

--- lib/src/main/java/com/diffplug/spotless/PaddedCellBulk.java
+++ lib/src/main/java/com/diffplug/spotless/PaddedCellBulk.java
@@ -27,7 +27,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
