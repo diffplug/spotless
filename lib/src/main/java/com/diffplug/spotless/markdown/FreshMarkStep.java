a060ee4d05836b313416466ee686fc17a2d1dede
Ned Twigg <ned.twigg@diffplug.com>
2016-12-07 01:23:02 -0800

    Added FreshMark to lib, along with infrastructure necessary to test steps entirely within lib.

---
+++ lib/src/main/java/com/diffplug/spotless/markdown/FreshMarkStep.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.spotless.markdown;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.net.URLClassLoader;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+import com.diffplug.spotless.FormatterFunc;
+import com.diffplug.spotless.FormatterStep;
+import com.diffplug.spotless.JarState;
+import com.diffplug.spotless.Provisioner;
+import com.diffplug.spotless.ThrowingEx.Supplier;
+
+/** A step for [FreshMark](https://github.com/diffplug/freshmark). */
+public class FreshMarkStep {
+	public static final String defaultVersion() {
+		return DEFAULT_VERSION;
+	}
+
+	private static final String DEFAULT_VERSION = "1.3.1";
+	private static final String NAME = "freshmark";
+	private static final String MAVEN_COORDINATE = "com.diffplug.freshmark:freshmark:";
+	private static final String FORMATTER_CLASS = "com.diffplug.freshmark.FreshMark";
+	private static final String FORMATTER_METHOD = "compile";
+
+	/** Creates a formatter step for the given version and settings file. */
+	public static FormatterStep create(Supplier<Map<String, ?>> properties, Provisioner provisioner) {
+		return create(defaultVersion(), properties, provisioner);
+	}
+
+	/** Creates a formatter step for the given version and settings file. */
+	public static FormatterStep create(String version, Supplier<Map<String, ?>> properties, Provisioner provisioner) {
+		return FormatterStep.createCloseableLazy(NAME,
+				() -> new State(new JarState(MAVEN_COORDINATE + version, provisioner), properties.get()),
+				State::createFormat);
+	}
+
+	private static class State implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		/** The jar that contains the eclipse formatter. */
+		final JarState jarState;
+		final Map<String, ?> properties;
+
+		State(JarState jarState, Map<String, ?> properties) {
+			this.jarState = Objects.requireNonNull(jarState);
+			this.properties = Objects.requireNonNull(properties);
+		}
+
+		FormatterFunc.Closeable createFormat() throws Exception {
+			Logger logger = Logger.getLogger(FreshMarkStep.class.getName());
+			Consumer<String> loggingStream = logger::warning;
+
+			URLClassLoader classLoader = jarState.openIsolatedClassLoader();
+
+			// instantiate the formatter and get its format method
+			Class<?> formatterClazz = classLoader.loadClass(FORMATTER_CLASS);
+			Object formatter = formatterClazz.getConstructor(Map.class, Consumer.class).newInstance(properties, loggingStream);
+			Method method = formatterClazz.getMethod(FORMATTER_METHOD, String.class);
+			return FormatterFunc.Closeable.of(classLoader, input -> (String) method.invoke(formatter, input));
+		}
+	}
+}
---
+++ testlib/src/main/java/com/diffplug/spotless/TestProvisioner.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.spotless;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
+import org.gradle.testfixtures.ProjectBuilder;
+
+import com.diffplug.common.base.StandardSystemProperty;
+
+public class TestProvisioner {
+	/**
+	 * Creates a Provisioner for the given repositories.
+	 *
+	 * The first time a project is created, there are ~7 seconds of configuration
+	 * which will go away for all subsequent runs.
+	 *
+	 * Every call to resolve will take about 1 second, even when all artifacts are resolved.
+	 */
+	private static Provisioner createWithRepositories(Consumer<RepositoryHandler> repoConfig) {
+		// use the default gradle home directory to ensure that files are always resolved to the same location
+		File gradleHome = new File(StandardSystemProperty.USER_DIR.value() + "/.gradle");
+		Project project = ProjectBuilder.builder()
+				.withGradleUserHomeDir(gradleHome)
+				.build();
+		repoConfig.accept(project.getRepositories());
+		return mavenCoords -> {
+			Dependency[] deps = mavenCoords.stream()
+					.map(project.getDependencies()::create)
+					.toArray(Dependency[]::new);
+			Configuration config = project.getConfigurations().detachedConfiguration(deps);
+			config.setDescription(mavenCoords.toString());
+			return config.resolve();
+		};
+	}
+
+	/** Creates a Provisioner for the jcenter repo. */
+	public static Provisioner jcenter() {
+		return createWithRepositories(repo -> repo.jcenter());
+	}
+
+	/** Creates a Provisioner for the mavenCentral repo. */
+	public static Provisioner mavenCentral() {
+		return createWithRepositories(repo -> repo.mavenCentral());
+	}
+}
