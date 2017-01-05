43457a7ab707799c31c81f781260b52ec7348d7f
Ned Twigg <ned.twigg@diffplug.com>
2017-01-05 07:53:45 -0800

    Moved GoogleJavaFormatTest into testlib.

--- testlib/src/test/java/com/diffplug/spotless/java/GoogleJavaFormatTest.java
+++ testlib/src/test/java/com/diffplug/spotless/java/GoogleJavaFormatTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.diffplug.spotless.extra.java;
+package com.diffplug.spotless.java;
 
 import org.junit.Test;
 
