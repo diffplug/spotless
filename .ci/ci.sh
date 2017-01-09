28c8af51146b0b8b17cc499db2c64a3a6fc8adae
Ned Twigg <ned.twigg@diffplug.com>
2017-01-09 13:30:36 -0800

    Publishing now calls generatePomFileForPluginMavenPublication first, to ensure that all POM are accurate.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -4,6 +4,8 @@
 ./gradlew build || exit 1
 
 if [ "$TRAVIS_REPO_SLUG" == "diffplug/spotless" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "3.x" ]; then
+	# Make sure that all pom are up-to-date
+	./gradlew generatePomFileForPluginMavenPublication
 	# Publish the artifacts
 	./gradlew publish publishPlugins -Dgradle.publish.key=$gradle_key -Dgradle.publish.secret=$gradle_secret || exit 1
 	# Push the javadoc
