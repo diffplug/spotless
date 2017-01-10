4a723315a51610cfa2e4605622f510b4745c4502
Ned Twigg <ned.twigg@diffplug.com>
2017-01-09 16:13:51 -0800

    Adapted CI to master.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -3,7 +3,7 @@
 # Do the Gradle build
 ./gradlew build || exit 1
 
-if [ "$TRAVIS_REPO_SLUG" == "diffplug/spotless" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "3.x" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "diffplug/spotless" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 	# Make sure that all pom are up-to-date
 	./gradlew generatePomFileForPluginMavenPublication
 	# Publish the artifacts
