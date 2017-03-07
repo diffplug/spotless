f26f305dca4f3b3ed4e2448b172d41719d11063c
Frank Vennemeyer <fvgh@users.noreply.github.com>
2017-03-07 10:30:01 +0100

    Feature/groovy test enhancements (#80)
    * Minor improvement to TestProvisioner's error message
    * Added TestProvisioner.mavenLocal(), which doesn't cache like the other TestProvisioner implementations.

--- testlib/src/main/java/com/diffplug/spotless/TestProvisioner.java
+++ testlib/src/main/java/com/diffplug/spotless/TestProvisioner.java
@@ -27,6 +27,7 @@ import java.util.function.Supplier;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ResolveException;
 import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.testfixtures.ProjectBuilder;
 
@@ -57,7 +58,12 @@ public class TestProvisioner {
 						.toArray(Dependency[]::new);
 				Configuration config = project.getConfigurations().detachedConfiguration(deps);
 				config.setDescription(mavenCoords.toString());
-				return config.resolve();
+				try {
+					return config.resolve();
+				} catch (ResolveException e) {
+					/* Provide Maven coordinates in exception message instead of static string 'detachedConfiguration' */
+					throw new ResolveException(config.getDescription(), e);
+				}
 			};
 		});
 	}
@@ -113,4 +119,11 @@ public class TestProvisioner {
 	private static final Supplier<Provisioner> mavenCentral = Suppliers.memoize(() -> {
 		return caching("mavenCentral", createLazyWithRepositories(repo -> repo.mavenCentral()));
 	});
+
+	/** Creates a Provisioner for the local maven repo for development purpose. */
+	public static Provisioner mavenLocal() {
+		return mavenLocal.get();
+	}
+
+	private static final Supplier<Provisioner> mavenLocal = createLazyWithRepositories(repo -> repo.mavenLocal());
 }
