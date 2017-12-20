17021791fe7b6b840a7cf5dc8a28d0b13fbffb00
Baptiste Mesta <baptiste.mesta@gmail.com>
2017-12-20 10:33:49 +0100

    PR Review

--- testlib/src/test/java/com/diffplug/spotless/generic/LicenseHeaderStepTest.java
+++ testlib/src/test/java/com/diffplug/spotless/generic/LicenseHeaderStepTest.java
@@ -32,11 +32,11 @@ public class LicenseHeaderStepTest extends ResourceHarness {
 	private static final String KEY_LICENSE = "license/TestLicense";
 	private static final String KEY_FILE_NOTAPPLIED = "license/MissingLicense.test";
 	private static final String KEY_FILE_APPLIED = "license/HasLicense.test";
-	private static final String KEY_LICENSE_WITH_YEAR = "license/TestLicencseWithYear";
-	private static final String KEY_FILE_WITHOUT_LICENSE = "license/MissLicenseWithYear.test";
-	private static final String KEY_FILE_WITH_PREVIOUS_YEAR = "license/LicenseWithPreviousYear.test";
-	private static final String KEY_FILE_WITH_PREVIOUS_YEARS = "license/LicenseWithPreviousYears.test";
-	private static final String KEY_FILE_WITH_CURRENT_YEAR = "license/LicenseWithYear.test";
+
+	// files to test $YEAR token replacement
+	private static final String KEY_LICENSE_WITH_YEAR_TOKEN = "license/LicenseHeaderWithYearToken";
+	private static final String KEY_FILE_WITHOUT_LICENSE = "license/FileWithoutLicenseHeader.test";
+	private static final String KEY_FILE_WITH_LICENSE_AND_PLACEHOLDER = "license/FileWithLicenseHeaderAndPlaceholder.test";
 
 	// If this constant changes, don't forget to change the similarly-named one in
 	// plugin-gradle/src/main/java/com/diffplug/gradle/spotless/JavaExtension.java as well
@@ -55,19 +55,19 @@ public class LicenseHeaderStepTest extends ResourceHarness {
 	}
 
 	@Test
-	public void should_apply_license_containing_YEAR_variable() throws Throwable {
-		FormatterStep step = LicenseHeaderStep.createFromFile(createTestFile(KEY_LICENSE_WITH_YEAR), StandardCharsets.UTF_8, LICENSE_HEADER_DELIMITER);
+	public void should_apply_license_containing_YEAR_token() throws Throwable {
+		FormatterStep step = LicenseHeaderStep.createFromFile(createTestFile(KEY_LICENSE_WITH_YEAR_TOKEN), StandardCharsets.UTF_8, LICENSE_HEADER_DELIMITER);
 
 		StepHarness.forStep(step)
-				.test(getTestResource(KEY_FILE_WITHOUT_LICENSE), getFileContentWithYEAR(currentYear()))
-				.testUnaffected(getFileContentWithYEAR(currentYear()))
-				.testUnaffected(getFileContentWithYEAR("2003"))
-				.testUnaffected(getFileContentWithYEAR("1990-2015"))
-				.test(getFileContentWithYEAR("not a year"), getFileContentWithYEAR(currentYear()));
+				.test(getTestResource(KEY_FILE_WITHOUT_LICENSE), fileWithPlaceholderContaining(currentYear()))
+				.testUnaffected(fileWithPlaceholderContaining(currentYear()))
+				.testUnaffected(fileWithPlaceholderContaining("2003"))
+				.testUnaffected(fileWithPlaceholderContaining("1990-2015"))
+				.test(fileWithPlaceholderContaining("not a year"), fileWithPlaceholderContaining(currentYear()));
 	}
 
-	private String getFileContentWithYEAR(String year) throws IOException {
-		return getTestResource(KEY_FILE_WITH_CURRENT_YEAR).replace("__YEAR_to_replace_in_tests__", year);
+	private String fileWithPlaceholderContaining(String placeHolderContent) throws IOException {
+		return getTestResource(KEY_FILE_WITH_LICENSE_AND_PLACEHOLDER).replace("__PLACEHOLDER__", placeHolderContent);
 	}
 
 	private String currentYear() {
