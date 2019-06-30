9f21178c18800b295f238d4d3b94ad327ed04923
Frank Vennemeyer <fvgh@users.noreply.github.com>
2019-06-30 13:41:49 +0200

    Support of OSGI framework wiring as require by Elipse internal platform since 4.12.

--- _ext/eclipse-base/src/main/java/com/diffplug/spotless/extra/eclipse/base/osgi/EclipseBundleLookup.java
+++ _ext/eclipse-base/src/main/java/com/diffplug/spotless/extra/eclipse/base/osgi/EclipseBundleLookup.java
@@ -15,26 +15,48 @@
  */
 package com.diffplug.spotless.extra.eclipse.base.osgi;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.eclipse.osgi.internal.framework.FilterImpl;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkListener;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.namespace.IdentityNamespace;
+import org.osgi.framework.wiring.BundleCapability;
+import org.osgi.framework.wiring.FrameworkWiring;
+import org.osgi.resource.Namespace;
+import org.osgi.resource.Requirement;
 import org.osgi.service.packageadmin.ExportedPackage;
 import org.osgi.service.packageadmin.PackageAdmin;
 import org.osgi.service.packageadmin.RequiredBundle;
 
 /**
- * {@link PackageAdmin} service for bundle look-up and bypassing wiring.
+ *
+ * {@link PackageAdmin} and {@link FrameworkWiring} service for bundle look-up.
  * <p>
- * The wiring information will always claim that all required bundles are present.
- * Other functionality is not supported.
- * Unsupported methods are marked as deprecated and throw an {@link UnsupportedOperationException}.
+ * The wiring information will always claim that all required bundles are present, since
+ * Spotlss does on purpose not provide all dependencies requested by plugins, since
+ * only small parts of the plugins are used.
+ * Removal and addition requests for bundles will always claim that there is nothing to do.
  * <p>
- * Interface is deprecated, but for example the InternalPlatform still uses PackageAdmin.
+ * PackageAdmin interface is deprecated, but might still be used by bundles.
+ * It is kept for backward compatibility until removed from Eclipse.
  */
 @SuppressWarnings("deprecation")
-class EclipseBundleLookup implements PackageAdmin {
+class EclipseBundleLookup implements FrameworkWiring, PackageAdmin {
 
+	private static final Set<String> OSGI_KEYS_FOR_SYMBOLIC_NAMES = Collections.unmodifiableSet(Stream.of(IdentityNamespace.IDENTITY_NAMESPACE, IdentityNamespace.TYPE_BUNDLE).collect(Collectors.toSet()));
+	private final Bundle systemBundle;
 	private final BundleSet bundles;
 
-	EclipseBundleLookup(final BundleSet bundles) {
+	EclipseBundleLookup(final Bundle systemBundle, final BundleSet bundles) {
+		this.systemBundle = systemBundle;
 		this.bundles = bundles;
 	}
 
@@ -97,4 +119,64 @@ class EclipseBundleLookup implements PackageAdmin {
 		return 0; //No fragments
 	}
 
+	@Override
+	public Bundle getBundle() {
+		return systemBundle;
+	}
+
+	@Override
+	public void refreshBundles(Collection<Bundle> bundles, FrameworkListener... listeners) {
+		//Spotless bundles cannot be loaded dynamically
+	}
+
+	@Override
+	public boolean resolveBundles(Collection<Bundle> bundles) {
+		return true;
+	}
+
+	@Override
+	public Collection<Bundle> getRemovalPendingBundles() {
+		return Collections.emptyList(); //Nothing to remove
+	}
+
+	@Override
+	public Collection<Bundle> getDependencyClosure(Collection<Bundle> bundles) {
+		return Collections.emptyList(); //No dependencies
+	}
+
+	@Override
+	public Collection<BundleCapability> findProviders(Requirement requirement) {
+		// requirement must not be null (according to interface description)!
+		String filterSpec = requirement.getDirectives().get(Namespace.REQUIREMENT_FILTER_DIRECTIVE);
+		if (null == filterSpec) {
+			throw new IllegalArgumentException("Requirement filter diretive '" + Namespace.REQUIREMENT_FILTER_DIRECTIVE + "' not found.");
+		}
+		try {
+			FilterImpl requirementFilter = FilterImpl.newInstance(filterSpec);
+			Collection<String> requiredSymbolicNames = getRequestedSymbolicNames(requirementFilter);
+			Collection<BundleCapability> capabilities = new ArrayList<BundleCapability>(requiredSymbolicNames.size());
+			requiredSymbolicNames.forEach(symbolicName -> {
+				Bundle bundle = bundles.get(symbolicName);
+				if (bundle != null) {
+					capabilities.add(new SimpleBundleCapability(bundle));
+				}
+			});
+			return capabilities;
+		} catch (InvalidSyntaxException e) {
+			throw new IllegalArgumentException("Filter specifiation invalid:\n" + filterSpec, e);
+		}
+	}
+
+	/**
+	 * Simplified parser irgnoreing the version.
+	 * Parser is incomplete since it ignores the filter operation.
+	 * It basicall implements the bespoke way Eclipse maps its old style bundle handling to OSGI.
+	 */
+	private static Collection<String> getRequestedSymbolicNames(FilterImpl filter) {
+		List<String> symbolicNames = filter.getStandardOSGiAttributes().entrySet().stream().filter(entry -> OSGI_KEYS_FOR_SYMBOLIC_NAMES.contains(entry.getKey())).map(entry -> entry.getValue()).collect(Collectors.toList());
+		filter.getChildren().forEach(childFilter -> {
+			symbolicNames.addAll(getRequestedSymbolicNames(childFilter));
+		});
+		return symbolicNames;
+	}
 }
