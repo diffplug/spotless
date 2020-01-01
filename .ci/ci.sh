1ecf40af3f92cd089105a2b97cd3c0cdac2d0a5b
Ned Twigg <ned.twigg@diffplug.com>
2020-01-01 15:38:08 -0800

    Remove the javadoc-publish project.

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -9,6 +9,4 @@ if [ "$TRAVIS_REPO_SLUG" == "diffplug/spotless" ] && [ "$TRAVIS_PULL_REQUEST" ==
 	./gradlew generatePomFileForPluginMavenPublication --build-cache
 	# Publish the artifacts
 	./gradlew publish publishPlugins -Dgradle.publish.key=$gradle_key -Dgradle.publish.secret=$gradle_secret --build-cache || exit 1
-	# Push the javadoc
-	./gradlew gitPublishPush --build-cache || exit 1
 fi
