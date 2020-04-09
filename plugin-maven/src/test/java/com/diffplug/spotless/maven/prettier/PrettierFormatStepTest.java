ef591ef14f183383a761ba66120e6f5d6f79b3a9
oliver-szymanski <oliver.szymanski@source-knights.com>
2020-04-09 22:15:22 +0100

    spotless apply

--- plugin-maven/src/test/java/com/diffplug/spotless/maven/prettier/PrettierFormatStepTest.java
+++ plugin-maven/src/test/java/com/diffplug/spotless/maven/prettier/PrettierFormatStepTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 DiffPlug
+ * Copyright 2016 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ public class PrettierFormatStepTest extends MavenIntegrationTest {
 		String suffix = "ts";
 		writePomWithPrettierSteps("**/*." + suffix,
 				"<prettier>",
-			"  <prettierVersion>1.16.4</prettierVersion>",
+				"  <prettierVersion>1.16.4</prettierVersion>",
 				"  <configFile>.prettierrc.yml</configFile>",
 				"</prettier>");
 		run("typescript", suffix);
