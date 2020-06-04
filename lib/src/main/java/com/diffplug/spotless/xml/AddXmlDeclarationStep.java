5fb5f6ccde3232eeec20885026290985766a6736
David Carver <kingargyle@gmail.com>
2020-06-04 19:51:30 -0400

    Fix formatting issues
    Reformats the AddXmlDeclaration class to project standards.

--- lib/src/main/java/com/diffplug/spotless/xml/AddXmlDeclarationStep.java
+++ lib/src/main/java/com/diffplug/spotless/xml/AddXmlDeclarationStep.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.diffplug.spotless.xml;
 
 import com.diffplug.spotless.FormatterStep;
@@ -9,8 +24,8 @@ public class AddXmlDeclarationStep {
 
 	public static FormatterStep create() {
 		return FormatterStep.create("addXmlDeclaration",
-			AddXmlDeclarationStep.class,
-			unused -> AddXmlDeclarationStep::format);
+				AddXmlDeclarationStep.class,
+				unused -> AddXmlDeclarationStep::format);
 	}
 
 	private static String format(String rawString) {
@@ -21,4 +36,3 @@ public class AddXmlDeclarationStep {
 		return replacement;
 	}
 }
-
