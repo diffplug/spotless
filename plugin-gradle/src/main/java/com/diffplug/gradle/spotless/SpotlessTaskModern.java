ba371deb5245481ae4922a04558a48e3e6d07d16
Ned Twigg <ned.twigg@diffplug.com>
2020-06-27 17:21:06 -0700

    Remove support for -PspotlessFiles from the modern task.  Big code reduction.

--- plugin-gradle/src/main/java/com/diffplug/gradle/spotless/SpotlessTaskModern.java
+++ plugin-gradle/src/main/java/com/diffplug/gradle/spotless/SpotlessTaskModern.java
@@ -16,12 +16,7 @@
 package com.diffplug.gradle.spotless;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import org.gradle.api.GradleException;
 import org.gradle.api.file.FileCollection;
@@ -35,9 +30,6 @@ import org.gradle.work.FileChange;
 import org.gradle.work.Incremental;
 import org.gradle.work.InputChanges;
 
-import com.diffplug.common.base.Errors;
-import com.diffplug.common.base.Preconditions;
-import com.diffplug.common.base.Throwing;
 import com.diffplug.spotless.Formatter;
 
 @CacheableTask
@@ -63,43 +55,14 @@ public class SpotlessTaskModern extends SpotlessTask {
 			Files.createDirectories(outputDirectory.toPath());
 		}
 
-		Throwing.Specific.Predicate<File, IOException> shouldInclude;
-		if (this.filePatterns.isEmpty()) {
-			shouldInclude = file -> true;
-		} else {
-			Preconditions.checkArgument(ratchet == null,
-					"Cannot use 'ratchetFrom' and '-PspotlessFiles' at the same time");
-
-			// a list of files has been passed in via project property
-			final String[] includePatterns = this.filePatterns.split(",");
-			final List<Pattern> compiledIncludePatterns = Arrays.stream(includePatterns)
-					.map(Pattern::compile)
-					.collect(Collectors.toList());
-			shouldInclude = file -> compiledIncludePatterns
-					.stream()
-					.anyMatch(filePattern -> filePattern.matcher(file.getAbsolutePath())
-							.matches());
-		}
-
 		try (Formatter formatter = buildFormatter()) {
 			for (FileChange fileChange : inputs.getFileChanges(target)) {
 				File input = fileChange.getFile();
 				if (fileChange.getChangeType() == ChangeType.REMOVED) {
-					try {
-						if (shouldInclude.test(input)) {
-							deletePreviousResult(input);
-						}
-					} catch (IOException e) {
-						throw Errors.asRuntime(e);
-					}
-
+					deletePreviousResult(input);
 				} else {
-					try {
-						if (shouldInclude.test(input) && input.isFile()) {
-							processInputFile(formatter, input);
-						}
-					} catch (IOException e) {
-						throw Errors.asRuntime(e);
+					if (input.isFile()) {
+						processInputFile(formatter, input);
 					}
 				}
 			}
