c63aa61b7a14fd0a3d874f89ac30c8b4b9b54fc9
Ned Twigg <ned.twigg@diffplug.com>
2020-07-01 21:35:33 -0700

    Do the rename in maven too.

--- plugin-maven/src/main/java/com/diffplug/spotless/maven/cpp/EclipseCdt.java
+++ plugin-maven/src/main/java/com/diffplug/spotless/maven/cpp/EclipseCdt.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 DiffPlug
+ * Copyright 2016-2020 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ import com.diffplug.spotless.extra.cpp.EclipseCdtFormatterStep;
 import com.diffplug.spotless.maven.FormatterStepConfig;
 import com.diffplug.spotless.maven.FormatterStepFactory;
 
-public class Eclipse implements FormatterStepFactory {
+public class EclipseCdt implements FormatterStepFactory {
 
 	@Parameter
 	private String file;
