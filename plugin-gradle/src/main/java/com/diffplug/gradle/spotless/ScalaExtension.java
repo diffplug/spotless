1bc5d3f0e0ccba686c9fdf0f2fb65539cafa9cd2
Ned Twigg <ned.twigg@diffplug.com>
2020-07-12 20:46:37 -0700

    Resolve wildcard import.

--- plugin-gradle/src/main/java/com/diffplug/gradle/spotless/ScalaExtension.java
+++ plugin-gradle/src/main/java/com/diffplug/gradle/spotless/ScalaExtension.java
@@ -16,7 +16,9 @@
 package com.diffplug.gradle.spotless;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
