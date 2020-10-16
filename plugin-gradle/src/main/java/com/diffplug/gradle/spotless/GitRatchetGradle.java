fa01f46bd1cb4dc74b78f8f8cea100ed3ac4e2ca
Ned Twigg <ned.twigg@diffplug.com>
2020-10-16 11:14:18 -0700

    Convert GitRatchetGradle into a BuildService, just to get around `buildFinished`, so that Spotless tasks don't interfere with config cache for other tasks.

--- plugin-gradle/src/main/java/com/diffplug/gradle/spotless/GitRatchetGradle.java
+++ plugin-gradle/src/main/java/com/diffplug/gradle/spotless/GitRatchetGradle.java
@@ -20,11 +20,13 @@ import java.io.File;
 import javax.annotation.Nullable;
 
 import org.gradle.api.Project;
+import org.gradle.api.services.BuildService;
+import org.gradle.api.services.BuildServiceParameters;
 
 import com.diffplug.spotless.extra.GitRatchet;
 
 /** Gradle implementation of GitRatchet. */
-final class GitRatchetGradle extends GitRatchet<Project> {
+public abstract class GitRatchetGradle extends GitRatchet<Project> implements BuildService<BuildServiceParameters.None> {
 	@Override
 	protected File getDir(Project project) {
 		return project.getProjectDir();
