de49bfece9f8818daf6bd3bf5d44860e6863e000
Ned Twigg <ned.twigg@diffplug.com>
2021-01-01 20:29:36 -0800

    FIxup formatting.

--- plugin-maven/src/main/java/com/diffplug/spotless/maven/kotlin/Ktfmt.java
+++ plugin-maven/src/main/java/com/diffplug/spotless/maven/kotlin/Ktfmt.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 DiffPlug
+ * Copyright 2016-2021 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ public class Ktfmt implements FormatterStepFactory {
 
 	@Parameter
 	private String version;
-	
+
 	@Parameter
 	private boolean dropboxStyle;
 
