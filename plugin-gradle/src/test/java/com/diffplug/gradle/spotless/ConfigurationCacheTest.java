87862619f03b702cacea03bcfa64eb7276211319
Ned Twigg <ned.twigg@diffplug.com>
2021-09-20 11:46:52 -0700

    Fix ConfigurationCacheTest to use JUnit5 (undetected merge problem from #720), it sucks that CircleCI doesn't automatically test merge commits in addition to merge tips.

--- plugin-gradle/src/test/java/com/diffplug/gradle/spotless/ConfigurationCacheTest.java
+++ plugin-gradle/src/test/java/com/diffplug/gradle/spotless/ConfigurationCacheTest.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ConfigurationCacheTest extends GradleIntegrationHarness {
 	protected void runTasks(String... tasks) throws IOException {
