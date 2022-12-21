cb3dd5536febc8fccb0b71ee1103225d6fd9eafb
Simon Gamma <github@survive.ch>
2022-12-21 16:28:32 +0100

    eslint: speedup npm install

--- lib/src/main/java/com/diffplug/spotless/npm/NpmProcess.java
+++ lib/src/main/java/com/diffplug/spotless/npm/NpmProcess.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 DiffPlug
+ * Copyright 2016-2022 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ class NpmProcess {
 	}
 
 	void install() {
-		npmAwait("install", "--no-audit", "--no-package-lock");
+		npmAwait("install", "--no-audit", "--no-package-lock", "--prefer-offline");
 	}
 
 	Process start() {
