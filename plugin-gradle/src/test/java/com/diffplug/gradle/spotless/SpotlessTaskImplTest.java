e579b74e5f7fb69cd1b5d23ae0be388001400ddc
Benoit Lacelle <benoit.lacelle@gmail.com>
2023-03-10 09:45:58 +0400

    Fix filePath for both Windows and Unix

--- plugin-gradle/src/test/java/com/diffplug/gradle/spotless/SpotlessTaskImplTest.java
+++ plugin-gradle/src/test/java/com/diffplug/gradle/spotless/SpotlessTaskImplTest.java
@@ -16,6 +16,7 @@
 package com.diffplug.gradle.spotless;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 import org.assertj.core.api.Assertions;
 import org.gradle.api.file.DirectoryProperty;
@@ -31,15 +32,15 @@ public class SpotlessTaskImplTest {
 		SpotlessTaskImpl task = Mockito.mock(SpotlessTaskImpl.class, Mockito.CALLS_REAL_METHODS);
 		Mockito.when(task.getLogger()).thenReturn(Mockito.mock(Logger.class));
 
-		File projectDir = new File("unitTests/projectDir");
+		File projectDir = Paths.get("unitTests","projectDir").toFile();
 		DirectoryProperty projectDirProperty = Mockito.mock(DirectoryProperty.class, Mockito.RETURNS_DEEP_STUBS);
 		Mockito.when(projectDirProperty.get().getAsFile()).thenReturn(projectDir);
 
 		Mockito.when(task.getProjectDir()).thenReturn(projectDirProperty);
 
-		File input = new File("unitTests/projectDir/someInput");
+		File input = Paths.get("unitTests","projectDir", "someInput").toFile();
 		Formatter formatter = Mockito.mock(Formatter.class);
 
-		Assertions.assertThatThrownBy(() -> task.processInputFile(null, formatter, input)).hasMessageContaining("unitTests/projectDir/someInput");
+		Assertions.assertThatThrownBy(() -> task.processInputFile(null, formatter, input)).hasMessageContaining(input.toString());
 	}
 }
