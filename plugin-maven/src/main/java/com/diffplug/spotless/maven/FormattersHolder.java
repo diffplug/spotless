aeb6098e4a21f37c10d0146325c00552c8ba265f
Ned Twigg <ned.twigg@diffplug.com>
2023-04-05 14:30:26 -0700

    spotlessApply

--- plugin-maven/src/main/java/com/diffplug/spotless/maven/FormattersHolder.java
+++ plugin-maven/src/main/java/com/diffplug/spotless/maven/FormattersHolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 DiffPlug
+ * Copyright 2021-2023 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package com.diffplug.spotless.maven;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
