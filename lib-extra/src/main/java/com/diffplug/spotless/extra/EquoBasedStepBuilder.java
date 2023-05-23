655bc7ba0c68d3d505fc670c174f6a2a0fc4b835
Ned Twigg <ned.twigg@diffplug.com>
2023-05-23 14:44:49 -0700

    Bump solstice and durian-swt to latest.

--- lib-extra/src/main/java/com/diffplug/spotless/extra/EquoBasedStepBuilder.java
+++ lib-extra/src/main/java/com/diffplug/spotless/extra/EquoBasedStepBuilder.java
@@ -110,8 +110,8 @@ public abstract class EquoBasedStepBuilder {
 		}
 		var classpath = new ArrayList<File>();
 		var mavenDeps = new ArrayList<String>();
-		mavenDeps.add("dev.equo.ide:solstice:1.0.3");
-		mavenDeps.add("com.diffplug.durian:durian-swt.os:4.1.1");
+		mavenDeps.add("dev.equo.ide:solstice:1.3.1");
+		mavenDeps.add("com.diffplug.durian:durian-swt.os:4.2.0");
 		mavenDeps.addAll(query.getJarsOnMavenCentral());
 		classpath.addAll(mavenProvisioner.provisionWithTransitives(false, mavenDeps));
 		classpath.addAll(query.getJarsNotOnMavenCentral());
