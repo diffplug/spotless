3754eb12d3da00be2da0cdadd3123e11ae63af9b
Simon Gamma <github@survive.ch>
2023-09-04 15:02:11 +0200

    style: fix import order

--- lib/src/main/java/com/diffplug/spotless/npm/ListableAdapter.java
+++ lib/src/main/java/com/diffplug/spotless/npm/ListableAdapter.java
@@ -15,12 +15,12 @@
  */
 package com.diffplug.spotless.npm;
 
-import javax.annotation.Nonnull;
-
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
+
+import javax.annotation.Nonnull;
 
 class ListableAdapter<T> implements Iterable<T> {
 
