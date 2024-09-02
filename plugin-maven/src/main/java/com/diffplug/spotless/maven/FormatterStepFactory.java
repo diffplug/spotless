482295d0183e4a02e5716fd983521f4cf16358fc
Kevin Leturc <kevinleturc@users.noreply.github.com>
2024-09-02 13:37:42 +0200

    Fix validation tests

--- plugin-maven/src/main/java/com/diffplug/spotless/maven/FormatterStepFactory.java
+++ plugin-maven/src/main/java/com/diffplug/spotless/maven/FormatterStepFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 DiffPlug
+ * Copyright 2016-2024 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
