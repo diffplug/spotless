195bc165ce94a2a9086736b28fb2c3dd06740e03
Ned Twigg <ned.twigg@diffplug.com>
2024-10-20 08:59:42 -0700

    Remove `testResourceExceptionMsg` and replace with `expectLintsOfResource`.

--- testlib/src/main/java/com/diffplug/spotless/StepHarness.java
+++ testlib/src/main/java/com/diffplug/spotless/StepHarness.java
@@ -21,9 +21,6 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-import org.assertj.core.api.AbstractStringAssert;
-import org.assertj.core.api.Assertions;
-
 import com.diffplug.selfie.Selfie;
 import com.diffplug.selfie.StringSelfie;
 
@@ -91,34 +88,26 @@ public class StepHarness extends StepHarnessBase {
 		return testUnaffected(idempotentElement);
 	}
 
-	public AbstractStringAssert<?> testResourceExceptionMsg(String resourceBefore) {
-		return testExceptionMsg(ResourceHarness.getTestResource(resourceBefore));
-	}
-
-	public AbstractStringAssert<?> testExceptionMsg(String before) {
-		try {
-			formatter().compute(LineEnding.toUnix(before), Formatter.NO_FILE_SENTINEL);
-			throw new SecurityException("Expected exception");
-		} catch (Throwable e) {
-			if (e instanceof SecurityException) {
-				throw new AssertionError(e.getMessage());
-			} else {
-				Throwable rootCause = e;
-				while (rootCause.getCause() != null) {
-					if (rootCause instanceof IllegalStateException) {
-						break;
-					}
-					rootCause = rootCause.getCause();
-				}
-				return Assertions.assertThat(rootCause.getMessage());
-			}
-		}
+	public StringSelfie expectLintsOfResource(String before) {
+		return expectLintsOf(ResourceHarness.getTestResource(before));
 	}
 
 	public StringSelfie expectLintsOf(String before) {
 		LintState state = LintState.of(formatter(), Formatter.NO_FILE_SENTINEL, before.getBytes(formatter().getEncoding()));
-		String assertAgainst = state.asString(Formatter.NO_FILE_SENTINEL, formatter());
+		return expectLintsOf(state, formatter());
+	}
+
+	static StringSelfie expectLintsOf(LintState state, Formatter formatter) {
+		String assertAgainst = state.asString(Formatter.NO_FILE_SENTINEL, formatter);
 		String cleaned = assertAgainst.replace("NO_FILE_SENTINEL:", "");
-		return Selfie.expectSelfie(cleaned);
+
+		int numLines = 1;
+		int lineEnding = cleaned.indexOf('\n');
+		while (lineEnding != -1 && numLines < 10) {
+			++numLines;
+			lineEnding = cleaned.indexOf('\n', lineEnding + 1);
+		}
+		String toSnapshot = lineEnding == -1 ? cleaned : (cleaned.substring(0, lineEnding) + "\n(... and more)");
+		return Selfie.expectSelfie(toSnapshot);
 	}
 }
