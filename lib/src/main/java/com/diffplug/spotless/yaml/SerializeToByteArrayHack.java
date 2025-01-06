168e5978bf4036893500115ca34acf38034fed49
Ned Twigg <ned.twigg@diffplug.com>
2025-01-06 09:34:50 -0800

    Add a sentinel step which triggers the "serialize to byte array hack"

---
+++ lib/src/main/java/com/diffplug/spotless/yaml/SerializeToByteArrayHack.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 DiffPlug
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.diffplug.spotless.yaml;
+
+import java.io.File;
+
+import javax.annotation.Nullable;
+
+import com.diffplug.spotless.FormatterStep;
+
+public class SerializeToByteArrayHack implements FormatterStep {
+	private static final long serialVersionUID = 8071047581828362545L;
+
+	@Override
+	public String getName() {
+		return "hack to force serializing objects to byte array";
+	}
+
+	@Nullable
+	@Override
+	public String format(String rawUnix, File file) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void close() throws Exception {
+
+	}
+}
