ca0c86bc7606a0166066ed41d2d549d273edf3b8
Goooler <wangzongler@gmail.com>
2025-07-03 20:39:15 +0800

    Migrate outdated APIs and bump the min Gradle requirement to 7.1

--- plugin-gradle/src/main/java/com/diffplug/gradle/spotless/JvmLang.java
+++ plugin-gradle/src/main/java/com/diffplug/gradle/spotless/JvmLang.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 DiffPlug
+ * Copyright 2023-2025 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,31 +22,20 @@ import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.SourceDirectorySet;
-import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
-import org.gradle.util.GradleVersion;
 
 interface JvmLang {
 
 	default FileCollection getSources(Project project, String message, Function<SourceSet, SourceDirectorySet> sourceSetSourceDirectory, Spec<? super File> filterSpec) {
-		final SourceSetContainer sourceSets;
 		FileCollection union = project.files();
-		if (GradleVersion.current().compareTo(GradleVersion.version(SpotlessPlugin.VER_GRADLE_javaPluginExtension)) >= 0) {
-			final JavaPluginExtension javaPluginExtension = project.getExtensions().findByType(JavaPluginExtension.class);
-			if (javaPluginExtension == null) {
-				throw new GradleException(message);
-			}
-			sourceSets = javaPluginExtension.getSourceSets();
-		} else {
-			final JavaPluginConvention javaPluginConvention = project.getConvention().findPlugin(JavaPluginConvention.class);
-			if (javaPluginConvention == null) {
-				throw new GradleException(message);
-			}
-			sourceSets = javaPluginConvention.getSourceSets();
+		final JavaPluginExtension javaPluginExtension = project.getExtensions().findByType(JavaPluginExtension.class);
+		if (javaPluginExtension == null) {
+			throw new GradleException(message);
 		}
+		final SourceSetContainer sourceSets = javaPluginExtension.getSourceSets();
 		for (SourceSet sourceSet : sourceSets) {
 			union = union.plus(sourceSetSourceDirectory.apply(sourceSet).filter(filterSpec));
 		}
