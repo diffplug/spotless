b6bcc36f23c25e57a35dd9918925e7cfaa80cf36
Goooler <wangzongler@gmail.com>
2025-10-09 14:35:29 +0800

    Update things

--- lib/src/main/java/com/diffplug/spotless/java/GoogleJavaFormatStep.java
+++ lib/src/main/java/com/diffplug/spotless/java/GoogleJavaFormatStep.java
@@ -123,7 +123,7 @@ public final class GoogleJavaFormatStep implements Serializable {
 			.addMin(11, "1.8") // we only support google-java-format >= 1.8 due to api changes
 			.addMin(16, "1.10.0") // java 16 requires at least 1.10.0 due to jdk api changes in JavaTokenizer
 			.addMin(21, "1.17.0") // java 21 requires at least 1.17.0 due to https://github.com/google/google-java-format/issues/898
-			.add(17, "1.28.0"); // default version
+			.add(21, "1.29.0"); // default version
 
 	public static String defaultGroupArtifact() {
 		return MAVEN_COORDINATE;
--- plugin-gradle/CHANGES.md
+++ plugin-gradle/CHANGES.md
@@ -3,6 +3,8 @@
 We adhere to the [keepachangelog](https://keepachangelog.com/en/1.0.0/) format (starting after version `3.27.0`).
 
 ## [Unreleased]
+### Changes
+* Bump default `google-java-format` version to latest `1.28.0` -> `1.29.0`. ([#2661](https://github.com/diffplug/spotless/pull/2661))
 
 ## [8.0.0] - 2025-09-24
 ### Changed
