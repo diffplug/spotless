dec96fea0f685eab1c8d943e20881ba01a921b1a
Simon Le Bras <lebras.simon@gmail.com>
2025-11-13 16:32:08 +0100

    Eagerly resolve IDE hook properties

--- plugin-gradle/src/main/java/com/diffplug/gradle/spotless/IdeHook.java
+++ plugin-gradle/src/main/java/com/diffplug/gradle/spotless/IdeHook.java
@@ -19,8 +19,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import javax.annotation.Nullable;
+
 import org.gradle.api.Project;
-import org.gradle.api.provider.Provider;
+import org.gradle.api.provider.ProviderFactory;
 
 import com.diffplug.common.base.Errors;
 import com.diffplug.common.io.ByteStreams;
@@ -30,22 +32,20 @@ import com.diffplug.spotless.NoLambda;
 
 final class IdeHook {
 	static class State extends NoLambda.EqualityBasedOnSerialization {
-		final Provider<String> path;
-		final Provider<Boolean> useStdIn;
-		final Provider<Boolean> useStdOut;
+		final @Nullable String path;
+		final boolean useStdIn;
+		final boolean useStdOut;
 
 		State(Project project) {
-			this.path = project.getProviders().gradleProperty(PROPERTY);
-
-			this.useStdIn = path.map(path -> project.getProviders()
-					.gradleProperty(USE_STD_IN)
-					.isPresent())
-					.orElse(false);
-
-			this.useStdOut = path.map(path -> project.getProviders()
-					.gradleProperty(USE_STD_OUT)
-					.isPresent())
-					.orElse(false);
+			ProviderFactory providers = project.getProviders();
+			path = providers.gradleProperty(PROPERTY).getOrNull();
+			if (path != null) {
+				useStdIn = providers.gradleProperty(USE_STD_IN).isPresent();
+				useStdOut = providers.gradleProperty(USE_STD_OUT).isPresent();
+			} else {
+				useStdIn = false;
+				useStdOut = false;
+			}
 		}
 	}
 
@@ -58,7 +58,7 @@ final class IdeHook {
 	}
 
 	static void performHook(SpotlessTaskImpl spotlessTask, IdeHook.State state) {
-		File file = new File(state.path.get());
+		File file = new File(state.path);
 		if (!file.isAbsolute()) {
 			System.err.println("Argument passed to " + PROPERTY + " must be an absolute path");
 			return;
@@ -73,7 +73,7 @@ final class IdeHook {
 					}
 				}
 				byte[] bytes;
-				if (state.useStdIn.get()) {
+				if (state.useStdIn) {
 					bytes = ByteStreams.toByteArray(System.in);
 				} else {
 					bytes = Files.readAllBytes(file.toPath());
@@ -86,7 +86,7 @@ final class IdeHook {
 					System.err.println("Run 'spotlessDiagnose' for details https://github.com/diffplug/spotless/blob/main/PADDEDCELL.md");
 				} else {
 					System.err.println("IS DIRTY");
-					if (state.useStdOut.get()) {
+					if (state.useStdOut) {
 						dirty.writeCanonicalTo(System.out);
 					} else {
 						dirty.writeCanonicalTo(file);
