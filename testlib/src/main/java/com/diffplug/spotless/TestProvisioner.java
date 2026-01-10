c131cac02bb49be349ad4ca9d8d94ac8ac2206cd
Vincent Potucek <vpotucek@me.com>
2026-01-10 20:14:01 +0100

    [rewrite] add `CodeCleanup`
    Signed-off-by: Vincent Potucek <vpotucek@me.com>
    
    - https://docs.openrewrite.org/recipes/staticanalysis/codecleanup
    
    ```
    There were problems parsing lib/src/main/resources/com/diffplug/spotless/npm/eslint-package.json
    There were problems parsing lib/src/main/resources/com/diffplug/spotless/npm/prettier-package.json
    There were problems parsing lib/src/main/resources/com/diffplug/spotless/npm/tsfmt-package.json
    All sources parsed, running active recipes: com.diffplug.spotless.openrewrite.SanityCheck
    Changes have been made to testlib/src/main/java/com/diffplug/spotless/TestProvisioner.java by:
        org.openrewrite.staticanalysis.HideUtilityClassConstructor
    Changes have been made to testlib/src/main/java/com/diffplug/spotless/ReflectionUtil.java by:
        org.openrewrite.staticanalysis.HideUtilityClassConstructor
    Changes have been made to testlib/src/test/java/com/diffplug/spotless/PaddedCellTest.java by:
        org.openrewrite.staticanalysis.ForLoopControlVariablePostfixOperators
    Please review and commit the results.
    Estimate time saved: 15m
    ```

--- testlib/src/main/java/com/diffplug/spotless/TestProvisioner.java
+++ testlib/src/main/java/com/diffplug/spotless/TestProvisioner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2025 DiffPlug
+ * Copyright 2026 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,33 @@
  */
 package com.diffplug.spotless;
 
+import static com.diffplug.common.base.Errors.asRuntime;
+import static com.diffplug.common.base.StandardSystemProperty.USER_DIR;
+import static com.diffplug.common.base.StandardSystemProperty.USER_HOME;
+import static com.diffplug.common.base.Suppliers.memoize;
+import static com.diffplug.common.collect.ImmutableSet.copyOf;
+import static com.diffplug.common.io.Files.asByteSink;
+import static com.diffplug.common.io.Files.asByteSource;
+import static com.diffplug.common.io.Files.createParentDirs;
+import static com.diffplug.common.io.Files.createTempDir;
+import static java.nio.file.Files.walk;
+import static java.nio.file.Path.of;
+import static java.util.Comparator.reverseOrder;
+import static java.util.Objects.requireNonNull;
+import static org.gradle.api.attributes.Bundling.BUNDLING_ATTRIBUTE;
+import static org.gradle.api.attributes.Bundling.EXTERNAL;
+import static org.gradle.api.attributes.Category.CATEGORY_ATTRIBUTE;
+import static org.gradle.api.attributes.Category.LIBRARY;
+import static org.gradle.api.attributes.java.TargetJvmEnvironment.STANDARD_JVM;
+import static org.gradle.api.attributes.java.TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE;
+import static org.gradle.testfixtures.ProjectBuilder.builder;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Path;
-import java.util.Comparator;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -34,125 +55,135 @@ import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.attributes.Bundling;
 import org.gradle.api.attributes.Category;
 import org.gradle.api.attributes.java.TargetJvmEnvironment;
-import org.gradle.testfixtures.ProjectBuilder;
 
-import com.diffplug.common.base.Errors;
-import com.diffplug.common.base.StandardSystemProperty;
-import com.diffplug.common.base.Suppliers;
 import com.diffplug.common.collect.ImmutableSet;
-import com.diffplug.common.io.Files;
 
-public class TestProvisioner {
-	public static Project gradleProject(File dir) {
-		File userHome = new File(StandardSystemProperty.USER_HOME.value());
-		return ProjectBuilder.builder()
-				.withGradleUserHomeDir(new File(userHome, ".gradle"))
-				.withProjectDir(dir)
+public final class TestProvisioner {
+
+	private static final String MAVEN_CENTRAL_CACHE = "build/tmp/testprovisioner.mavenCentral.cache";
+	private static final String TEST_LIB = "testlib";
+
+	private TestProvisioner() {}
+
+	public static Project gradleProject(File projectDir) {
+		return builder()
+				.withGradleUserHomeDir(new File(requireNonNull(USER_HOME.value()), ".gradle"))
+				.withProjectDir(projectDir)
 				.build();
 	}
 
 	/**
-	 * Creates a Provisioner for the given repositories.
-	 * <p>
-	 * The first time a project is created, there are ~7 seconds of configuration
-	 * which will go away for all subsequent runs.
-	 * <p>
-	 * Every call to resolve will take about 1 second, even when all artifacts are resolved.
+	 * Creates a Provisioner for the mavenCentral repo.
 	 */
-	private static Provisioner createWithRepositories(Consumer<RepositoryHandler> repoConfig) {
-		// Running this takes ~3 seconds the first time it is called. Probably because of classloading.
-		File tempDir = Files.createTempDir();
-		Project project = TestProvisioner.gradleProject(tempDir);
-		repoConfig.accept(project.getRepositories());
-		return (withTransitives, mavenCoords) -> {
-			Dependency[] deps = mavenCoords.stream()
-					.map(project.getDependencies()::create)
-					.toArray(Dependency[]::new);
-			Configuration config = project.getConfigurations().detachedConfiguration(deps);
-			config.setTransitive(withTransitives);
-			config.setDescription(mavenCoords.toString());
-			config.attributes(attr -> {
-				attr.attribute(Category.CATEGORY_ATTRIBUTE, project.getObjects().named(Category.class, Category.LIBRARY));
-				attr.attribute(Bundling.BUNDLING_ATTRIBUTE, project.getObjects().named(Bundling.class, Bundling.EXTERNAL));
-				// Add this attribute for resolving Guava dependency, see https://github.com/google/guava/issues/6801.
-				attr.attribute(TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE, project.getObjects().named(TargetJvmEnvironment.class, TargetJvmEnvironment.STANDARD_JVM));
-			});
-			try {
-				return config.resolve();
-			} catch (ResolveException e) {
-				/* Provide Maven coordinates in exception message instead of static string 'detachedConfiguration' */
-				throw new RuntimeException("Error resolving configuration: " + config.getDescription(), e);
-			} finally {
-				// delete the temp dir
-				try {
-					java.nio.file.Files.walk(tempDir.toPath())
-							.sorted(Comparator.reverseOrder())
-							.map(Path::toFile)
-							.forEach(File::delete);
-				} catch (IOException e) {
-					throw Errors.asRuntime(e);
-				}
-			}
-		};
+	public static Provisioner mavenCentral() {
+		return memoize(() -> caching(() -> createWithRepositories(RepositoryHandler::mavenCentral))).get();
 	}
 
-	/** Creates a Provisioner which will cache the result of previous calls. */
-	@SuppressWarnings("unchecked")
-	private static Provisioner caching(String name, Supplier<Provisioner> input) {
-		File spotlessDir = new File(StandardSystemProperty.USER_DIR.value()).getParentFile();
-		File testlib = new File(spotlessDir, "testlib");
-		File cacheFile = new File(testlib, "build/tmp/testprovisioner." + name + ".cache");
+	/**
+	 * Creates a Provisioner which will cache the result of previous calls.
+	 */
+	private static Provisioner caching(Supplier<Provisioner> delegate) {
+		var cacheFile = new File(new File(new File(requireNonNull(USER_DIR.value())).getParentFile(), TEST_LIB), MAVEN_CENTRAL_CACHE);
+		return (withTransitives, rawCoords) -> resolveAndCache(
+				delegate,
+				withTransitives,
+				loadCache(cacheFile),
+				copyOf(rawCoords),
+				cacheFile);
+	}
 
-		Map<ImmutableSet<String>, ImmutableSet<File>> cached;
-		if (cacheFile.exists()) {
-			try (ObjectInputStream inputStream = new ObjectInputStream(Files.asByteSource(cacheFile).openBufferedStream())) {
-				cached = (Map<ImmutableSet<String>, ImmutableSet<File>>) inputStream.readObject();
-			} catch (IOException | ClassNotFoundException e) {
-				throw Errors.asRuntime(e);
+	private static ImmutableSet<File> resolveAndCache(Supplier<Provisioner> delegate,
+			boolean withTransitives,
+			Map<ImmutableSet<String>, ImmutableSet<File>> artifactCache,
+			ImmutableSet<String> mavenCoords,
+			File cacheFile) {
+		synchronized (TestProvisioner.class) {
+			var cachedFiles = artifactCache.get(mavenCoords);
+			var cacheValid = cachedFiles != null &&
+					cachedFiles.stream().allMatch(
+							f -> f.exists() && f.isFile() && f.length() > 0);
+			if (!cacheValid) {
+				cachedFiles = copyOf(
+						delegate.get()
+								.provisionWithTransitives(withTransitives, mavenCoords));
+				artifactCache.put(mavenCoords, cachedFiles);
+				try (var out = new ObjectOutputStream(asByteSink(cacheFile).openBufferedStream())) {
+					out.writeObject(artifactCache);
+				} catch (IOException e) {
+					throw asRuntime(e);
+				}
 			}
-		} else {
-			cached = new HashMap<>();
-			try {
-				Files.createParentDirs(cacheFile);
-			} catch (IOException e) {
-				throw Errors.asRuntime(e);
+			return cachedFiles;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static Map<ImmutableSet<String>, ImmutableSet<File>> loadCache(File cacheFile) {
+		if (cacheFile.exists()) {
+			try (var in = new ObjectInputStream(asByteSource(cacheFile).openBufferedStream())) {
+				return (Map<ImmutableSet<String>, ImmutableSet<File>>) in.readObject();
+			} catch (IOException | ClassNotFoundException e) {
+				throw asRuntime(e);
 			}
 		}
-		return (withTransitives, mavenCoordsRaw) -> {
-			ImmutableSet<String> mavenCoords = ImmutableSet.copyOf(mavenCoordsRaw);
-			synchronized (TestProvisioner.class) {
-				ImmutableSet<File> result = cached.get(mavenCoords);
-				// double-check that depcache pruning hasn't removed them since our cache cached them
-				boolean needsToBeSet = result == null || !result.stream().allMatch(file -> file.exists() && file.isFile() && file.length() > 0);
-				if (needsToBeSet) {
-					result = ImmutableSet.copyOf(input.get().provisionWithTransitives(withTransitives, mavenCoords));
-					cached.put(mavenCoords, result);
-					try (ObjectOutputStream outputStream = new ObjectOutputStream(Files.asByteSink(cacheFile).openBufferedStream())) {
-						outputStream.writeObject(cached);
-					} catch (IOException e) {
-						throw Errors.asRuntime(e);
-					}
-				}
-				return result;
+		try {
+			createParentDirs(cacheFile);
+			return new HashMap<>();
+		} catch (IOException e) {
+			throw asRuntime(e);
+		}
+	}
+
+	/**
+	 * Creates a Provisioner for the given repositories.
+	 */
+	private static Provisioner createWithRepositories(Consumer<RepositoryHandler> repositoryConfigurer) {
+		return createProvisioner(repositoryConfigurer, gradleProject(createTempDir()));
+	}
+
+	private static Provisioner createProvisioner(Consumer<RepositoryHandler> repositoryConfigurer, Project project) {
+		repositoryConfigurer.accept(project.getRepositories());
+		return (withTransitives, mavenCoords) -> {
+			var configuration = applyAttributes(project
+					.getConfigurations()
+					.detachedConfiguration(mavenCoords
+							.stream()
+							.map(project.getDependencies()::create)
+							.toArray(Dependency[]::new)),
+					withTransitives,
+					mavenCoords,
+					project);
+			try {
+				return configuration.resolve();
+			} catch (ResolveException e) {
+				throw new RuntimeException("Error resolving configuration: " + configuration.getDescription(), e);
+			} finally {
+				try (var paths = walk(of(project.getPath()))) {
+					paths.sorted(reverseOrder())
+							.map(Path::toFile)
+							.forEach(File::delete);
+				} catch (IOException ignored) {}
 			}
 		};
 	}
 
-	/** Creates a Provisioner for the mavenCentral repo. */
-	public static Provisioner mavenCentral() {
-		return MAVEN_CENTRAL.get();
+	private static Configuration applyAttributes(Configuration configuration,
+			boolean withTransitives,
+			Collection<String> mavenCoords,
+			Project project) {
+		configuration.setTransitive(withTransitives);
+		configuration.setDescription(mavenCoords.toString());
+		configuration.attributes(attrs -> {
+			attrs.attribute(
+					CATEGORY_ATTRIBUTE,
+					project.getObjects().named(Category.class, LIBRARY));
+			attrs.attribute(
+					BUNDLING_ATTRIBUTE,
+					project.getObjects().named(Bundling.class, EXTERNAL));
+			attrs.attribute(
+					TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+					project.getObjects().named(TargetJvmEnvironment.class, STANDARD_JVM));
+		});
+		return configuration;
 	}
-
-	private static final Supplier<Provisioner> MAVEN_CENTRAL = Suppliers.memoize(() -> caching("mavenCentral", () -> createWithRepositories(RepositoryHandler::mavenCentral)));
-
-	/** Creates a Provisioner for the local maven repo for development purpose. */
-	public static Provisioner mavenLocal() {
-		return createWithRepositories(RepositoryHandler::mavenLocal);
-	}
-
-	/** Creates a Provisioner for the Sonatype snapshots maven repo for development purpose. */
-	public static Provisioner snapshots() {
-		return createWithRepositories(repo -> repo.maven(setup -> setup.setUrl("https://oss.sonatype.org/content/repositories/snapshots")));
-	}
-
 }
