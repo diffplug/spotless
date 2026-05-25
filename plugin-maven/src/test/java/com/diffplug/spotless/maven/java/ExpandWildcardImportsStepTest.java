88b4f02604a955e3a754b89711ee1d618ec23eba
tavori <230928651+tavori@users.noreply.github.com>
2026-05-25 18:37:11 +0800

    fix: use string block syntax
    improve code

--- plugin-maven/src/test/java/com/diffplug/spotless/maven/java/ExpandWildcardImportsStepTest.java
+++ plugin-maven/src/test/java/com/diffplug/spotless/maven/java/ExpandWildcardImportsStepTest.java
@@ -32,12 +32,16 @@ class ExpandWildcardImportsStepTest extends MavenIntegrationHarness {
 
 		// Create supporting classes in source roots so JavaParserTypeSolver can resolve them
 		setFile("src/main/java/foo/bar/AnotherClassInSamePackage.java")
-				.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
+			.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
 		setFile("src/main/java/foo/bar/baz/AnotherImportedClass.java")
-				.toResource("java/expandwildcardimports/AnotherImportedClass.test");
+			.toResource("java/expandwildcardimports/AnotherImportedClass.test");
 		// Source for the annotation used in the test (resolves via source root, not JAR)
 		setFile("src/main/java/org/example/SomeAnnotation.java")
-				.toContent("package org.example;\n\npublic @interface SomeAnnotation {}\n");
+			.toContent("""
+						package org.example;
+
+						public @interface SomeAnnotation {}
+						""");
 
 		String path = "src/main/java/foo/bar/JavaClassWithWildcards.java";
 		setFile(path).toResource("java/expandwildcardimports/JavaClassWithWildcardsUnformatted.test");
@@ -55,19 +59,20 @@ class ExpandWildcardImportsStepTest extends MavenIntegrationHarness {
 	void expandWildcardImports_withDirectJarDependency() throws Exception {
 		setupLocalMavenRepo();
 
-		String projectDeps =
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-annotation-lib</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"    </dependency>\n";
+		String projectDeps = """
+				<dependency>
+				  <groupId>com.example</groupId>
+				  <artifactId>test-annotation-lib</artifactId>
+				  <version>1.0.0</version>
+				</dependency>
+				""";
 
 		writePomWithProjectDeps("<java><expandWildcardImports/></java>", projectDeps);
 
 		setFile("src/main/java/foo/bar/AnotherClassInSamePackage.java")
-				.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
+			.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
 		setFile("src/main/java/foo/bar/baz/AnotherImportedClass.java")
-				.toResource("java/expandwildcardimports/AnotherImportedClass.test");
+			.toResource("java/expandwildcardimports/AnotherImportedClass.test");
 
 		String path = "src/main/java/foo/bar/JavaClassWithWildcards.java";
 		setFile(path).toResource("java/expandwildcardimports/JavaClassWithWildcardsUnformatted.test");
@@ -85,21 +90,22 @@ class ExpandWildcardImportsStepTest extends MavenIntegrationHarness {
 	void expandWildcardImports_withPomTypeDependency() throws Exception {
 		setupLocalMavenRepo();
 
-		String projectDeps =
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-dependencies</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"      <type>pom</type>\n" +
-				"    </dependency>\n";
+		String projectDeps = """
+				<dependency>
+				  <groupId>com.example</groupId>
+				  <artifactId>test-dependencies</artifactId>
+				  <version>1.0.0</version>
+				  <type>pom</type>
+				</dependency>
+				""";
 
 		writePomWithProjectDeps("<java><expandWildcardImports/></java>", projectDeps);
 
 		// Create supporting classes in source roots
 		setFile("src/main/java/foo/bar/AnotherClassInSamePackage.java")
-				.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
+			.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
 		setFile("src/main/java/foo/bar/baz/AnotherImportedClass.java")
-				.toResource("java/expandwildcardimports/AnotherImportedClass.test");
+			.toResource("java/expandwildcardimports/AnotherImportedClass.test");
 		// SomeAnnotation comes from the JAR (transitively via POM dependency), not from source
 
 		String path = "src/main/java/foo/bar/JavaClassWithWildcards.java";
@@ -120,13 +126,14 @@ class ExpandWildcardImportsStepTest extends MavenIntegrationHarness {
 	void expandWildcardImports_withPomDependencyTransitiveJars() throws Exception {
 		setupLocalMavenRepo();
 
-		String projectDeps =
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-dependencies</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"      <type>pom</type>\n" +
-				"    </dependency>\n";
+		String projectDeps = """
+				<dependency>
+				  <groupId>com.example</groupId>
+				  <artifactId>test-dependencies</artifactId>
+				  <version>1.0.0</version>
+				  <type>pom</type>
+				</dependency>
+				""";
 
 		writePomWithProjectDeps("<java><expandWildcardImports/></java>", projectDeps);
 
@@ -149,25 +156,26 @@ class ExpandWildcardImportsStepTest extends MavenIntegrationHarness {
 	void expandWildcardImports_withMixedPomAndJarDependencies() throws Exception {
 		setupLocalMavenRepo();
 
-		String projectDeps =
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-dependencies</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"      <type>pom</type>\n" +
-				"    </dependency>\n" +
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-annotation-lib</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"    </dependency>\n";
+		String projectDeps = """
+				<dependency>
+				  <groupId>com.example</groupId>
+				  <artifactId>test-dependencies</artifactId>
+				  <version>1.0.0</version>
+				  <type>pom</type>
+				</dependency>
+				<dependency>
+				  <groupId>com.example</groupId>
+				  <artifactId>test-annotation-lib</artifactId>
+				  <version>1.0.0</version>
+				</dependency>
+				""";
 
 		writePomWithProjectDeps("<java><expandWildcardImports/></java>", projectDeps);
 
 		setFile("src/main/java/foo/bar/AnotherClassInSamePackage.java")
-				.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
+			.toResource("java/expandwildcardimports/AnotherClassInSamePackage.test");
 		setFile("src/main/java/foo/bar/baz/AnotherImportedClass.java")
-				.toResource("java/expandwildcardimports/AnotherImportedClass.test");
+			.toResource("java/expandwildcardimports/AnotherImportedClass.test");
 
 		String path = "src/main/java/foo/bar/JavaClassWithWildcards.java";
 		setFile(path).toResource("java/expandwildcardimports/JavaClassWithWildcardsUnformatted.test");
@@ -179,110 +187,112 @@ class ExpandWildcardImportsStepTest extends MavenIntegrationHarness {
 
 	private void setupLocalMavenRepo() throws Exception {
 		// Write a minimal POM so that mvnw can start for install:install-file
-		setFile("pom.xml").toContent(
-				"<project xmlns=\"http://maven.apache.org/POM/4.0.0\">\n" +
-				"  <modelVersion>4.0.0</modelVersion>\n" +
-				"  <groupId>tmp</groupId>\n" +
-				"  <artifactId>tmp</artifactId>\n" +
-				"  <version>1.0</version>\n" +
-				"</project>\n");
+		setFile("pom.xml").toContent("""
+				<project xmlns="http://maven.apache.org/POM/4.0.0">
+				  <modelVersion>4.0.0</modelVersion>
+				  <groupId>tmp</groupId>
+				  <artifactId>tmp</artifactId>
+				  <version>1.0</version>
+				</project>
+				""");
 
 		// Copy JARs using binary I/O (setFile().toResource() uses text I/O which corrupts binary files)
 		copyBinaryResource("java/expandwildcardimports/example-lib.jar", "test-annotation-lib.jar");
 		copyBinaryResource("java/expandwildcardimports/example-model.jar", "test-model-lib.jar");
 
-		String aggregatorPomContent =
-				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-				"<project xmlns=\"http://maven.apache.org/POM/4.0.0\"\n" +
-				"         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-				"         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
-				"  <modelVersion>4.0.0</modelVersion>\n" +
-				"  <groupId>com.example</groupId>\n" +
-				"  <artifactId>test-dependencies</artifactId>\n" +
-				"  <version>1.0.0</version>\n" +
-				"  <packaging>pom</packaging>\n" +
-				"  <dependencies>\n" +
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-annotation-lib</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"    </dependency>\n" +
-				"    <dependency>\n" +
-				"      <groupId>com.example</groupId>\n" +
-				"      <artifactId>test-model-lib</artifactId>\n" +
-				"      <version>1.0.0</version>\n" +
-				"    </dependency>\n" +
-				"  </dependencies>\n" +
-				"</project>\n";
+		String aggregatorPomContent = """
+				<?xml version="1.0" encoding="UTF-8"?>
+				<project xmlns="http://maven.apache.org/POM/4.0.0"
+				         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+				         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+				  <modelVersion>4.0.0</modelVersion>
+				  <groupId>com.example</groupId>
+				  <artifactId>test-dependencies</artifactId>
+				  <version>1.0.0</version>
+				  <packaging>pom</packaging>
+				  <dependencies>
+				    <dependency>
+				      <groupId>com.example</groupId>
+				      <artifactId>test-annotation-lib</artifactId>
+				      <version>1.0.0</version>
+				    </dependency>
+				    <dependency>
+				      <groupId>com.example</groupId>
+				      <artifactId>test-model-lib</artifactId>
+				      <version>1.0.0</version>
+				    </dependency>
+				  </dependencies>
+				</project>
+				""";
 		setFile("test-dependencies-pom.xml").toContent(aggregatorPomContent);
 
 		// Install artifacts into the local Maven repo so transitive resolution works
 		mavenRunner().withArguments(
-				"install:install-file",
-				"-Dfile=test-annotation-lib.jar",
-				"-DgroupId=com.example",
-				"-DartifactId=test-annotation-lib",
-				"-Dversion=1.0.0",
-				"-Dpackaging=jar").runNoError();
+			"install:install-file",
+			"-Dfile=test-annotation-lib.jar",
+			"-DgroupId=com.example",
+			"-DartifactId=test-annotation-lib",
+			"-Dversion=1.0.0",
+			"-Dpackaging=jar").runNoError();
 
 		mavenRunner().withArguments(
-				"install:install-file",
-				"-Dfile=test-model-lib.jar",
-				"-DgroupId=com.example",
-				"-DartifactId=test-model-lib",
-				"-Dversion=1.0.0",
-				"-Dpackaging=jar").runNoError();
+			"install:install-file",
+			"-Dfile=test-model-lib.jar",
+			"-DgroupId=com.example",
+			"-DartifactId=test-model-lib",
+			"-Dversion=1.0.0",
+			"-Dpackaging=jar").runNoError();
 
 		mavenRunner().withArguments(
-				"install:install-file",
-				"-Dfile=test-dependencies-pom.xml",
-				"-DgroupId=com.example",
-				"-DartifactId=test-dependencies",
-				"-Dversion=1.0.0",
-				"-Dpackaging=pom").runNoError();
+			"install:install-file",
+			"-Dfile=test-dependencies-pom.xml",
+			"-DgroupId=com.example",
+			"-DartifactId=test-dependencies",
+			"-Dversion=1.0.0",
+			"-Dpackaging=pom").runNoError();
 	}
 
 	private void writePomWithProjectDeps(String spotlessConfig, String projectDependencies) throws Exception {
 		String spotlessVersion = System.getProperty("spotlessMavenPluginVersion");
-		String pomContent =
-				"<project xmlns=\"http://maven.apache.org/POM/4.0.0\" " +
-				"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
-				"xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
-				"  <modelVersion>4.0.0</modelVersion>\n" +
-				"  <groupId>com.diffplug.spotless</groupId>\n" +
-				"  <artifactId>spotless-maven-plugin-tests</artifactId>\n" +
-				"  <version>1.0.0-SNAPSHOT</version>\n" +
-				"  <prerequisites>\n" +
-				"    <maven>3.1.0</maven>\n" +
-				"  </prerequisites>\n" +
-				"  <properties>\n" +
-				"    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>\n" +
-				"    <maven.compiler.source>1.8</maven.compiler.source>\n" +
-				"    <maven.compiler.target>1.8</maven.compiler.target>\n" +
-				"  </properties>\n" +
-				"  <dependencies>\n" +
-				projectDependencies +
-				"  </dependencies>\n" +
-				"  <build>\n" +
-				"    <plugins>\n" +
-				"      <plugin>\n" +
-				"        <groupId>com.diffplug.spotless</groupId>\n" +
-				"        <artifactId>spotless-maven-plugin</artifactId>\n" +
-				"        <version>" + spotlessVersion + "</version>\n" +
-				"        <configuration>\n" +
-				"          " + spotlessConfig + "\n" +
-				"        </configuration>\n" +
-				"      </plugin>\n" +
-				"    </plugins>\n" +
-				"  </build>\n" +
-				"</project>\n";
+		String pomContent = """
+				<project xmlns="http://maven.apache.org/POM/4.0.0"
+				         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+				         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+				  <modelVersion>4.0.0</modelVersion>
+				  <groupId>com.diffplug.spotless</groupId>
+				  <artifactId>spotless-maven-plugin-tests</artifactId>
+				  <version>1.0.0-SNAPSHOT</version>
+				  <prerequisites>
+				    <maven>3.1.0</maven>
+				  </prerequisites>
+				  <properties>
+				    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+				    <maven.compiler.source>1.8</maven.compiler.source>
+				    <maven.compiler.target>1.8</maven.compiler.target>
+				  </properties>
+				  <dependencies>
+				    %s
+				  </dependencies>
+				  <build>
+				    <plugins>
+				      <plugin>
+				        <groupId>com.diffplug.spotless</groupId>
+				        <artifactId>spotless-maven-plugin</artifactId>
+				        <version>%s</version>
+				        <configuration>
+				          %s
+				        </configuration>
+				      </plugin>
+				    </plugins>
+				  </build>
+				</project>
+				""".formatted(projectDependencies, spotlessVersion, spotlessConfig);
 		setFile("pom.xml").toContent(pomContent);
 	}
 
 	private void copyBinaryResource(String resourcePath, String targetName) throws Exception {
-		byte[] bytes = Resources.toByteArray(ResourceHarness.class.getResource("/" + resourcePath));
 		Path target = newFile(targetName).toPath();
 		Files.createDirectories(target.getParent());
-		Files.write(target, bytes);
+		Files.write(target, Resources.toByteArray(ResourceHarness.class.getResource("/" + resourcePath)));
 	}
 }
