0d39b4d7745764f41b028ca47bbe40b722494f3a
Jochen Berger <fooberger@gmail.com>
2026-05-29 12:08:40 +0200

    desperate measures...

--- lib-extra/src/main/java/com/diffplug/spotless/extra/EquoBasedStepBuilder.java
+++ lib-extra/src/main/java/com/diffplug/spotless/extra/EquoBasedStepBuilder.java
@@ -125,6 +125,8 @@ public abstract class EquoBasedStepBuilder {
 			P2Model model = createModelWithMirrors();
 			P2ModelWrapper modelWrapper = P2ModelWrapper.wrap(model);
 			List<File> classpath = p2Provisioner.provisionP2Dependencies(modelWrapper, mavenProvisioner, cacheDirectory);
+			// work around https://github.com/equodev/equo-ide/pull/193
+			classpath = classpath.stream().filter(f -> !f.getName().contains("groovy-test")).toList();
 			return JarState.preserveOrder(classpath);
 		}), stepProperties.build());
 		return FormatterStep.create(formatterName, roundtrippableState, EquoStep::state, stateToFormatter);
